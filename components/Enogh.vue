06eb888590dc1fe09ee2387dae2ee7ad6c9c876f
ziggywizard <rippiten23@gmail.com>
2024-07-23 09:17:10 +0300

    Enough section parsed to component

---
+++ components/Enogh.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen w-screen flex flex-col text-center justify-evenly">
+    <h2 class="text-[72px]">
+      Isn't it enough for fake relationships, fake emotions, fake lifes, fake
+      stuffs?
+    </h2>
+    <button class="underline inline-block bg-white text-black p-1">
+      stop this thing, and start to
+      <a class="underline inline-block bg-white text-black p-1">live now.</a>
+    </button>
+  </div>
+</template>
