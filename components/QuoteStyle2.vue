52592eebf6273ceb4f60b2a57399220c7c33f254
ziggywizard <rippiten23@gmail.com>
2024-07-23 09:29:39 +0300

    static assets removed

--- components/QuoteStyle2.vue
+++ components/QuoteStyle2.vue
@@ -6,7 +6,10 @@
         more lonely."
       </h1>
       <video autoplay muted loop class="w-full max-h-screen object-cover w-1/2">
-        <source src="/seengirl.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dyhcjtotx/video/upload/v1721715547/seengirl_ocyzmm.mp4"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
     </div>
