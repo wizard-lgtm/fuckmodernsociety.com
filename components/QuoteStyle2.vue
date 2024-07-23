f8c022109ffec354cc97e0c6c006bf44416344f7
ziggywizard <rippiten23@gmail.com>
2024-07-23 09:17:34 +0300

    Quote seciton turned to two components diffrent styles, i didn't decide yet

---
+++ components/QuoteStyle2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen min-w-screen flex flex-col text-center">
+    <div class="flex flex-col md:flex-row">
+      <h1 class="text-[72px] font-bold w-1/2">
+        "The world has never been more connected, yet the people never have been
+        more lonely."
+      </h1>
+      <video autoplay muted loop class="w-full max-h-screen object-cover w-1/2">
+        <source src="/seengirl.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+</template>
